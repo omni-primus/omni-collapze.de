f89c3032682fb8e7592daf3402d8e016567029a6
Paul Krampe <doa2wsw@yahoo.de>
2023-10-16 00:20:45 +0200

    mobil drag, mobile styling

--- packages/tournament/Resources/Public/Js/tournament.js
+++ packages/tournament/Resources/Public/Js/tournament.js
@@ -1,4 +1,4 @@
-const eles = document.querySelectorAll('.tx-tournament .container');
+const eles = document.querySelectorAll('.tx-tournament > div');
 
 let pos = { top: 0, left: 0, x: 0, y: 0 };
 
@@ -18,6 +18,32 @@ const mouseDownHandler = function (e) {
     this.addEventListener('mouseup', mouseUpHandler);
 };
 
+const touchStartHandler = function (e) {
+    pos = {
+        // The current scroll
+        left: this.scrollLeft,
+        top: this.scrollTop,
+        // Get the current mouse position
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY,
+    };
+
+    this.addEventListener('touchmove', touchDragHandler);
+    this.addEventListener('touchend', touchEndHandler);
+}
+
+const touchDragHandler = function (e) {
+    const dx = e.changedTouches[0].clientX - pos.x;
+    const dy = e.changedTouches[0].clientY - pos.y;
+
+    // Scroll the element
+    this.scrollTop = pos.top - dy;
+    this.scrollLeft = pos.left - dx;
+}
+
+const touchEndHandler = function (e) {
+}
+
 const mouseMoveHandler = function (e) {
     // How far the mouse has been moved
     const dx = e.clientX - pos.x;
@@ -30,13 +56,15 @@ const mouseMoveHandler = function (e) {
 
 const mouseUpHandler = function () {
     this.removeEventListener('mousemove', mouseMoveHandler);
+    this.removeEventListener('touchmove', mouseMoveHandler);
     this.removeEventListener('mouseup', mouseUpHandler);
+    this.removeEventListener('touchend', mouseUpHandler);
 
     this.style.cursor = 'grab';
     this.style.removeProperty('user-select');
 };
 
 for(let i = 0;i<eles.length;i++) {
-    console.log(eles[i]);
     eles[i].addEventListener('mousedown', mouseDownHandler);
+    eles[i].addEventListener('touchstart', touchStartHandler);
 }
