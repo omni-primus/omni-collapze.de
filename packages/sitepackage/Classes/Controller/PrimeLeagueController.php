1041ad66f62623bdd2642eff69a52bedef17622d
omni primus <doa2wsw@yahoo.de>
2025-06-27 19:54:32 +0200

    prime league table

--- packages/sitepackage/Classes/Controller/PrimeLeagueController.php
+++ packages/sitepackage/Classes/Controller/PrimeLeagueController.php
@@ -8,28 +8,41 @@ use function Safe\ps_add_pdflink;
 class PrimeLeagueController extends ActionController {
     public function showAction()
     {
-        $html = file_get_contents('https://www.primeleague.gg/de/leagues/prm/3187-summer-split-202425/group/509-gruppenphase/5660-gruppe-5-20');
-        $dom = new \DOMDocument();
-        $dom->loadHTML($html);
-        $xpath = new \DOMXPath($dom);
-        $rows = $xpath->query('//table[contains(@class, "table-ranking")]//tr');
+        $url = 'https://www.primeleague.gg/de/leagues/prm/3187-summer-split-202425/group/509-gruppenphase/5660-gruppe-5-20';
 
-        $teams = [];
-
-        foreach ($rows as $row) {
-            $cells = $row->getElementsByTagName('td');
-            if($cells->length == 0) {
-                continue;
-            }
-
-            $teamdata = [
-                'rank' => trim($cells->item(0)->textContent),
-                'name' => preg_replace('/\R.*/', '', trim($cells->item(1)->textContent)),
-                'score' => trim($cells->item(2)->textContent),
-            ];
-            $teams[] = $teamdata;
+        try {
+            $teams = $this->fetchHtml($url);
+            $this->view->assign('primeLeagueTable', $teams);
+        } catch (\Exception $e) {
+            echo 'Fehler: ' . $e->getMessage();
         }
-        $this->view->assign('primeLeagueTable', $teams);
         return $this->htmlResponse();
+    }
+
+    function fetchHtml($url) {
+        if($url) {
+            $html = file_get_contents($url);
+            $dom = new \DOMDocument();
+            $dom->loadHTML($html);
+            $xpath = new \DOMXPath($dom);
+            $rows = $xpath->query('//table[contains(@class, "table-ranking")]//tr');
+
+            $teams = [];
+
+            foreach ($rows as $row) {
+                $cells = $row->getElementsByTagName('td');
+                if($cells->length == 0) {
+                    continue;
+                }
+
+                $teamdata = [
+                    'rank' => trim($cells->item(0)->textContent),
+                    'name' => preg_replace('/\R.*/', '', trim($cells->item(1)->textContent)),
+                    'score' => trim($cells->item(2)->textContent),
+                ];
+                $teams[] = $teamdata;
+            }
+        }
+        return $teams;
     }
 }
