8cd84d7d4099250a5609a6c9c7e455d03d3b32e8
Paul Henker <p.henker@prodat-sql.de>
2026-05-16 14:38:08 +0200

    scrim add draft tab

--- packages/sitepackage/Classes/Domain/Model/ScrimGame.php
+++ packages/sitepackage/Classes/Domain/Model/ScrimGame.php
@@ -21,6 +21,46 @@ class ScrimGame extends AbstractEntity
 
     protected string $winner = '';
 
+    protected string $blueBan1 = '';
+
+    protected string $blueBan2 = '';
+
+    protected string $blueBan3 = '';
+
+    protected string $redBan1 = '';
+
+    protected string $redBan2 = '';
+
+    protected string $redBan3 = '';
+
+    protected string $bluePick1 = '';
+
+    protected string $bluePick2 = '';
+
+    protected string $bluePick3 = '';
+
+    protected string $redPick1 = '';
+
+    protected string $redPick2 = '';
+
+    protected string $redPick3 = '';
+
+    protected string $blueBan4 = '';
+
+    protected string $blueBan5 = '';
+
+    protected string $redBan4 = '';
+
+    protected string $redBan5 = '';
+
+    protected string $bluePick4 = '';
+
+    protected string $bluePick5 = '';
+
+    protected string $redPick4 = '';
+
+    protected string $redPick5 = '';
+
     /**
      * @var ObjectStorage<ScrimPlayer>
      */
@@ -99,6 +139,206 @@ class ScrimGame extends AbstractEntity
     public function setWinner(string $winner): void
     {
         $this->winner = $winner;
+    }
+
+    public function getBlueBan1(): string
+    {
+        return $this->blueBan1;
+    }
+
+    public function setBlueBan1(string $blueBan1): void
+    {
+        $this->blueBan1 = $blueBan1;
+    }
+
+    public function getBlueBan2(): string
+    {
+        return $this->blueBan2;
+    }
+
+    public function setBlueBan2(string $blueBan2): void
+    {
+        $this->blueBan2 = $blueBan2;
+    }
+
+    public function getBlueBan3(): string
+    {
+        return $this->blueBan3;
+    }
+
+    public function setBlueBan3(string $blueBan3): void
+    {
+        $this->blueBan3 = $blueBan3;
+    }
+
+    public function getRedBan1(): string
+    {
+        return $this->redBan1;
+    }
+
+    public function setRedBan1(string $redBan1): void
+    {
+        $this->redBan1 = $redBan1;
+    }
+
+    public function getRedBan2(): string
+    {
+        return $this->redBan2;
+    }
+
+    public function setRedBan2(string $redBan2): void
+    {
+        $this->redBan2 = $redBan2;
+    }
+
+    public function getRedBan3(): string
+    {
+        return $this->redBan3;
+    }
+
+    public function setRedBan3(string $redBan3): void
+    {
+        $this->redBan3 = $redBan3;
+    }
+
+    public function getBluePick1(): string
+    {
+        return $this->bluePick1;
+    }
+
+    public function setBluePick1(string $bluePick1): void
+    {
+        $this->bluePick1 = $bluePick1;
+    }
+
+    public function getBluePick2(): string
+    {
+        return $this->bluePick2;
+    }
+
+    public function setBluePick2(string $bluePick2): void
+    {
+        $this->bluePick2 = $bluePick2;
+    }
+
+    public function getBluePick3(): string
+    {
+        return $this->bluePick3;
+    }
+
+    public function setBluePick3(string $bluePick3): void
+    {
+        $this->bluePick3 = $bluePick3;
+    }
+
+    public function getRedPick1(): string
+    {
+        return $this->redPick1;
+    }
+
+    public function setRedPick1(string $redPick1): void
+    {
+        $this->redPick1 = $redPick1;
+    }
+
+    public function getRedPick2(): string
+    {
+        return $this->redPick2;
+    }
+
+    public function setRedPick2(string $redPick2): void
+    {
+        $this->redPick2 = $redPick2;
+    }
+
+    public function getRedPick3(): string
+    {
+        return $this->redPick3;
+    }
+
+    public function setRedPick3(string $redPick3): void
+    {
+        $this->redPick3 = $redPick3;
+    }
+
+    public function getBlueBan4(): string
+    {
+        return $this->blueBan4;
+    }
+
+    public function setBlueBan4(string $blueBan4): void
+    {
+        $this->blueBan4 = $blueBan4;
+    }
+
+    public function getBlueBan5(): string
+    {
+        return $this->blueBan5;
+    }
+
+    public function setBlueBan5(string $blueBan5): void
+    {
+        $this->blueBan5 = $blueBan5;
+    }
+
+    public function getRedBan4(): string
+    {
+        return $this->redBan4;
+    }
+
+    public function setRedBan4(string $redBan4): void
+    {
+        $this->redBan4 = $redBan4;
+    }
+
+    public function getRedBan5(): string
+    {
+        return $this->redBan5;
+    }
+
+    public function setRedBan5(string $redBan5): void
+    {
+        $this->redBan5 = $redBan5;
+    }
+
+    public function getBluePick4(): string
+    {
+        return $this->bluePick4;
+    }
+
+    public function setBluePick4(string $bluePick4): void
+    {
+        $this->bluePick4 = $bluePick4;
+    }
+
+    public function getBluePick5(): string
+    {
+        return $this->bluePick5;
+    }
+
+    public function setBluePick5(string $bluePick5): void
+    {
+        $this->bluePick5 = $bluePick5;
+    }
+
+    public function getRedPick4(): string
+    {
+        return $this->redPick4;
+    }
+
+    public function setRedPick4(string $redPick4): void
+    {
+        $this->redPick4 = $redPick4;
+    }
+
+    public function getRedPick5(): string
+    {
+        return $this->redPick5;
+    }
+
+    public function setRedPick5(string $redPick5): void
+    {
+        $this->redPick5 = $redPick5;
     }
 
     /**
--- packages/sitepackage/Configuration/TCA/tx_sitepackage_domain_model_scrimgame.php
+++ packages/sitepackage/Configuration/TCA/tx_sitepackage_domain_model_scrimgame.php
@@ -26,8 +26,43 @@ return [
                 blue_side_team,
                 red_side_team,
                 winner,
+                --div--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.draft,
+                --palette--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.phase_bans_1;draftBansOne,
+                --palette--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.phase_picks_1;draftPicksOne,
+                --palette--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.phase_bans_2;draftBansTwo,
+                --palette--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.phase_picks_2;draftPicksTwo,
                 --div--;LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.players,
                 players
+            ',
+        ],
+    ],
+    'palettes' => [
+        'draftBansOne' => [
+            'showitem' => '
+                blue_ban_1, blue_ban_2, blue_ban_3,
+                --linebreak--,
+                red_ban_1, red_ban_2, red_ban_3
+            ',
+        ],
+        'draftPicksOne' => [
+            'showitem' => '
+                blue_pick_1, blue_pick_2, blue_pick_3,
+                --linebreak--,
+                red_pick_1, red_pick_2, red_pick_3
+            ',
+        ],
+        'draftBansTwo' => [
+            'showitem' => '
+                blue_ban_4, blue_ban_5,
+                --linebreak--,
+                red_ban_4, red_ban_5
+            ',
+        ],
+        'draftPicksTwo' => [
+            'showitem' => '
+                blue_pick_4, blue_pick_5,
+                --linebreak--,
+                red_pick_4, red_pick_5
             ',
         ],
     ],
@@ -101,6 +136,206 @@ return [
                 'renderType' => 'selectSingle',
                 'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ScrimTeamItemsProvider::class . '->addTeamItems',
                 'default' => 'omni',
+            ],
+        ],
+        'blue_ban_1' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_ban_1',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_ban_2' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_ban_2',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_ban_3' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_ban_3',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_ban_1' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_ban_1',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_ban_2' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_ban_2',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_ban_3' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_ban_3',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_pick_1' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_pick_1',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_pick_2' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_pick_2',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_pick_3' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_pick_3',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_pick_1' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_pick_1',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_pick_2' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_pick_2',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_pick_3' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_pick_3',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_ban_4' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_ban_4',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_ban_5' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_ban_5',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_ban_4' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_ban_4',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_ban_5' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_ban_5',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_pick_4' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_pick_4',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'blue_pick_5' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.blue_pick_5',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_pick_4' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_pick_4',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
+            ],
+        ],
+        'red_pick_5' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_db.xlf:tx_sitepackage_domain_model_scrimgame.red_pick_5',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [['', '']],
+                'itemsProcFunc' => \WebneoGmbh\Sitepackage\Backend\ChampionItemsProvider::class . '->addChampionItems',
             ],
         ],
         'players' => [
